c0360a42409fe5df9bb17ff8e6bcecf960ae1fb8
isaacasher97 <isaac.asher97@gmail.com>
2023-08-04 20:27:45 -0400

    Added useState and passed down as props + tested

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import Display from "./components/Display";
 import ControlledForm from "./components/ControlledForm";
+import { useState } from "react";
 
 function App() {
+
+  //create our state to hold the jservice data
+  const [question, setQuestion] = useState({})
+  //question is passed down to display as a prop
+
   return (
     <div className="App">
       <h1>Welcome To Jeapordy!</h1>
-      <ControlledForm />
-      <Display />
+      <ControlledForm setQuestion={setQuestion}/>
+      <Display question={question}/>
     </div>
   );
 }
