01a6206627424c79267dd94deca92c5f42d748b9
isaacasher97 <isaac.asher97@gmail.com>
2023-08-04 20:10:46 -0400

    First Commit, Components Exported Successfully

---
+++ src/components/ControlledForm.js
@@ -0,0 +1,5 @@
+function ControlledForm(props) {
+    return <h1>This is the Controlled Form Component</h1>
+}
+
+export default ControlledForm;
---
+++ src/components/Display.js
@@ -0,0 +1,5 @@
+function Display(props) {
+    return <h1>This is the Display Component</h1>
+}
+
+export default Display;
