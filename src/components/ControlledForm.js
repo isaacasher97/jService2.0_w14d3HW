c3ed542cb9fd4b7200759e928e600cdb6e46c2a5
isaacasher97 <isaac.asher97@gmail.com>
2023-08-04 20:51:22 -0400

    set up inputs on controlled form

--- src/components/ControlledForm.js
+++ src/components/ControlledForm.js
@@ -1,5 +1,17 @@
+import { useState } from "react";
+
+
 function ControlledForm(props) {
-    return <h1>This is the Controlled Form Component</h1>
+
+    //state for form data
+    const [formData, setFormData] = useState({})
+
+    return <div className="form">
+        <h2>Let's Play!</h2>
+        <form>
+            <input type="submit" value={"Get Question"}/>
+        </form>
+    </div>
 }
 
 export default ControlledForm;
