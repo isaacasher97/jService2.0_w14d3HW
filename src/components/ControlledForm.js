1a0bb34fa1f4600a1aa13414088780519fbf4b7e
isaacasher97 <isaac.asher97@gmail.com>
2023-08-06 12:47:38 -0400

    fixed and tested handleSubmit function

--- src/components/ControlledForm.js
+++ src/components/ControlledForm.js
@@ -5,10 +5,30 @@ function ControlledForm(props) {
 
     //state for form data
     const [formData, setFormData] = useState({})
+    //
+ 
+
+
+    //create a function to handle fetching from jService
+    const fetchJservice = async () => {
+     
+    const url = `http://jservice.io/api/random`
+    const response = await fetch(url);    
+    const data = await response.json()
+    props.setQuestion(data)
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        //create a copy of the current state
+        fetchJservice(formData)
+        setFormData({})
+    }
+ 
 
     return <div className="form">
         <h2>Let's Play!</h2>
-        <form>
+        <form onSubmit={handleSubmit}>
             <input type="submit" value={"Get Question"}/>
         </form>
     </div>
